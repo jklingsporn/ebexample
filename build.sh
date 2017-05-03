ecfd72b18108017eb54ad20381be5723fbd890b6
Jens Klingsporn <klingsporn@badmonkee.de>
2017-05-03 12:31:18 +0200

    remove profiles

--- build.sh
+++ build.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 set -e
-if [ -n "$1" ]; then
-        now=$(date +"%Y-%m-%d_%H-%M-%S")
-        tag="./target/app-$1-$now.zip"
-        mvn clean install -P$1
-        echo "web: ./run.sh" > target/Procfile
-        echo "exec java \$JAVA_OPTS -jar app-$1.jar \$JAVA_ARGS" > target/run.sh
-        chmod +x target/run.sh
-        zip -j ${tag} target/Procfile target/run.sh target/app-$1.jar
-    else
-        echo "Requires profile [local, dev, qa, prod]!"
-        exit 1
-fi
+now=$(date +"%Y-%m-%d_%H-%M-%S")
+tag="./target/app-$now.zip"
+mvn clean install
+echo "web: ./run.sh" > target/Procfile
+echo "exec java \$JAVA_OPTS -jar app.jar \$JAVA_ARGS" > target/run.sh
+chmod +x target/run.sh
+zip -j ${tag} target/Procfile target/run.sh target/app.jar
 
 
 
