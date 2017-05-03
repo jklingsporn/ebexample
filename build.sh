f41d638accc5dd8d15a17295954689d4366e3319
Jens Klingsporn <klingsporn@badmonkee.de>
2017-05-03 10:35:58 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+if [ -n "$1" ]; then
+        now=$(date +"%Y-%m-%d_%H-%M-%S")
+        tag="./target/app-$1-$now.zip"
+        mvn clean install -P$1
+        mkdir target
+        echo "web: ./run.sh" > target/Procfile
+        echo "exec java \$JAVA_OPTS -jar app-$1.jar \$JAVA_ARGS" > target/run.sh
+        chmod +x target/run.sh
+        zip -j ${tag} target/Procfile target/run.sh target/app-$1.jar
+    else
+        echo "Requires profile [local, dev, qa, prod]!"
+        exit 1
+fi
+
+
+
+
---
+++ src/main/java/io/github/jklingsporn/eb/Main.java
@@ -0,0 +1,14 @@
+package io.github.jklingsporn.eb;
+
+import java.util.Arrays;
+
+/**
+ * Created by jensklingsporn on 03.05.17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(String.format("Look at these wonderful arguments %s", Arrays.toString(args)));
+    }
+
+}
